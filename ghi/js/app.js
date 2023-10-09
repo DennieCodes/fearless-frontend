eb8a288f180c95a06cd0faddd19232710f812b45
Dennie <oculareo@gmail.com>
2023-10-09 15:04:37 -0400

    Added Fetch for conference name and description

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,27 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+	const url = 'http://localhost:8000/api/conferences/';
 
+	try {
+		const response = await fetch(url);
 
+		if (!response.ok) {
+			console.dir(response);
+		} else {
+			const data = await response.json();
+
+			const conference = data.conferences[0];
+			const nameTag = document.querySelector('.card-title');
+			nameTag.innerHTML = conference.name;
+
+			const detailUrl = `http://localhost:8000${conference.href}`;
+			const detailResponse = await fetch(detailUrl);
+			if (detailResponse.ok) {
+				const details = await detailResponse.json();
+				const descriptionTag = document.querySelector('.card-text');
+				descriptionTag.innerHTML = details.conference.description;
+			}
+		}
+	} catch (e) {
+		console.log('Error');
+	}
 });
