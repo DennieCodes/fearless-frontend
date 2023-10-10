bc629e17020ad858a4a4b24f95e9335ff5c2be4c
Dennie <oculareo@gmail.com>
2023-10-10 15:21:08 -0400

    Added form handling to post location submission

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', async () => {
+	// Fetch State data for select input
 	const url = 'http://localhost:8000/api/states/';
-
 	try {
 		const response = await fetch(url);
 
@@ -20,4 +20,27 @@ window.addEventListener('DOMContentLoaded', async () => {
 	} catch (e) {
 		console.error(e);
 	}
+
+	const formTag = document.getElementById('create-location-form');
+
+	formTag.addEventListener('submit', async (event) => {
+		event.preventDefault();
+		const formData = new FormData(formTag);
+		const json = JSON.stringify(Object.fromEntries(formData));
+
+		const locationUrl = 'http://localhost:8000/api/locations/';
+		const fetchConfig = {
+			method: 'post',
+			body: json,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		const response = await fetch(locationUrl, fetchConfig);
+		if (response.ok) {
+			formTag.reset();
+			const newLocation = await response.json();
+			console.log(newLocation);
+		}
+	});
 });
