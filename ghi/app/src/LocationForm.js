d4045d7d894700ad69b490943c5e7c9c6404b695
Dennie <oculareo@gmail.com>
2023-10-12 16:15:25 -0400

    Complete refactor of Location Form and page

--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 function LocationForm() {
 	const [states, setStates] = useState([]);
+	const [name, setName] = useState('');
+	const [roomCount, setRoomCount] = useState('');
+	const [city, setCity] = useState('');
+	const [state, setState] = useState('');
 
 	const fetchData = async () => {
 		const url = 'http://localhost:8000/api/states/';
@@ -19,6 +23,52 @@ function LocationForm() {
 		}
 	};
 
+	const handleNameChange = (event) => {
+		setName(event.target.value);
+	};
+
+	const handleRoomChange = (event) => {
+		setRoomCount(event.target.value);
+	};
+
+	const handleCityChange = (event) => {
+		setCity(event.target.value);
+	};
+
+	const handleStateChange = (event) => {
+		setState(event.target.value);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const data = {};
+		data.room_count = roomCount;
+		data.name = name;
+		data.city = city;
+		data.state = state;
+
+		const locationUrl = 'http://localhost:8000/api/locations/';
+		const fetchConfig = {
+			method: 'post',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		const response = await fetch(locationUrl, fetchConfig);
+		if (response.ok) {
+			const newLocation = await response.json();
+			console.log(newLocation);
+
+			setName('');
+			setRoomCount('');
+			setCity('');
+			setState('');
+		}
+	};
+
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -28,9 +78,11 @@ function LocationForm() {
 			<div className="offset-3 col-6">
 				<div className="shadow p-4 mt-4">
 					<h1>Create a new location</h1>
-					<form id="create-location-form">
+					<form id="create-location-form" onSubmit={handleSubmit}>
 						<div className="form-floating mb-3">
 							<input
+								onChange={handleNameChange}
+								value={name}
 								placeholder="Name"
 								required
 								type="text"
@@ -42,6 +94,8 @@ function LocationForm() {
 						</div>
 						<div className="form-floating mb-3">
 							<input
+								onChange={handleRoomChange}
+								value={roomCount}
 								placeholder="Room count"
 								required
 								type="number"
@@ -53,6 +107,8 @@ function LocationForm() {
 						</div>
 						<div className="form-floating mb-3">
 							<input
+								onChange={handleCityChange}
+								value={city}
 								placeholder="City"
 								required
 								type="text"
@@ -63,7 +119,14 @@ function LocationForm() {
 							<label htmlFor="city">City</label>
 						</div>
 						<div className="mb-3">
-							<select required id="state" name="state" className="form-select">
+							<select
+								onChange={handleStateChange}
+								value={state}
+								required
+								id="state"
+								name="state"
+								className="form-select"
+							>
 								<option value="">Choose a state</option>
 								{states.map((state) => {
 									return (
@@ -83,81 +146,3 @@ function LocationForm() {
 }
 
 export default LocationForm;
-
-// import React, { useEffect } from 'react';
-
-// function LocationForm() {
-// 	const fetchData = async () => {
-// 		const url = 'http://localhost:8000/api/states/';
-
-// 		const response = await fetch(url);
-
-// 		if (response.ok) {
-// 			const data = await response.json();
-
-// 			const selectTag = document.getElementById('state');
-// 			for (let state of data.states) {
-// 				const option = document.createElement('option');
-// 				option.value = state.abbreviation;
-// 				option.innerHTML = state.name;
-// 				selectTag.appendChild(option);
-// 			}
-// 		}
-// 	};
-
-// 	useEffect(() => {
-// 		fetchData();
-// 	}, []);
-// 	return (
-// 		<div className="row">
-// 			<div className="offset-3 col-6">
-// 				<div className="shadow p-4 mt-4">
-// 					<h1>Create a new location</h1>
-// 					<form id="create-location-form">
-// 						<div className="form-floating mb-3">
-// 							<input
-// 								placeholder="Name"
-// 								required
-// 								type="text"
-// 								name="name"
-// 								id="name"
-// 								className="form-control"
-// 							/>
-// 							<label htmlFor="name">Name</label>
-// 						</div>
-// 						<div className="form-floating mb-3">
-// 							<input
-// 								placeholder="Room count"
-// 								required
-// 								type="number"
-// 								name="room_count"
-// 								id="room_count"
-// 								className="form-control"
-// 							/>
-// 							<label htmlFor="room_count">Room count</label>
-// 						</div>
-// 						<div className="form-floating mb-3">
-// 							<input
-// 								placeholder="City"
-// 								required
-// 								type="text"
-// 								name="city"
-// 								id="city"
-// 								className="form-control"
-// 							/>
-// 							<label htmlFor="city">City</label>
-// 						</div>
-// 						<div className="mb-3">
-// 							<select required id="state" name="state" className="form-select">
-// 								<option value="">Choose a state</option>
-// 							</select>
-// 						</div>
-// 						<button className="btn btn-primary">Create</button>
-// 					</form>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-// export default LocationForm;
